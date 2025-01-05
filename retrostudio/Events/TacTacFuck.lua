07324399ac8363d5a5b712b1c29c57bc758bbb50
Anonymous <>
2025-01-05 22:06:03 +0900

    lol

---
+++ retrostudio/Events/TacTacFuck.lua
@@ -0,0 +1,24 @@
+function getType(istan:Instance, type_name:string, name:string):table
+    local type_tbl = {}
+    for _, even in istan:GetDescendants() do
+        if even:IsA(type_name) then
+            if name == nil then
+                table.insert(type_tbl, even)
+            elseif even.Name == name then
+                table.insert(type_tbl, even)
+            end
+        end
+    end
+    return type_tbl
+end
+
+local fag = workspace:GetDescendants()
+for _, lol in pairs(getType(game, "RemoteEvent", "Place")) do
+    lol:FireServer(unpack({
+        [1] = {
+            [1] = fag[math.random(0, #fag)],
+            [2] = fag[math.random(0, #fag)],
+            [3] = fag[math.random(0, #fag)]
+        }
+    }))
+end
