b5fde8ea7b202f9f4504620c102393c0eae07fbd
Anonymous <>
2025-01-09 23:51:03 +0900

    once done

--- retrostudio/FireTools.lua
+++ retrostudio/FireTools.lua
@@ -1,12 +1,12 @@
---[[
-    fireevent
-    mouseclick
-]]--
-local mode = "fireevent"
+local mode = 1
 local remoteFunc = false
 
 local PS = game:GetService("Players")
+local RS = game:GetService("ReplicatedStorage")
+
 local plrs = PS:GetPlayers()
+local Catalog = RS:FindFirstChild("CatalogItems")
+local CatalogGears = Catalog:FindFirstChild("Gears")
 
 function getRoot(char:Model):Part
 	return char:FindFirstChild('HumanoidRootPart') or char:FindFirstChild('Torso') or char:FindFirstChild('UpperTorso')
@@ -35,20 +35,22 @@ function inlinedRemote(e, args:table)
 end
 
 local callbacks = {}
-for _, v in game:GetDescendants() do
-    if string.lower(v.Name) == mode and v:IsA(remoteFunc and "RemoteFunction" or "RemoteEvent") then
+for _, v in CatalogGears:GetDescendants() do
+    if v:IsA(remoteFunc and "RemoteFunction" or "RemoteEvent") then
         table.insert(callbacks, v)
     end
 end
 
 for _, remote in pairs(callbacks) do
     local out
-    if mode == "fireevent" then
+    if mode == 1 then
         out = inlinedRemote(remote, {[1] = getRootSafe(false).Position})
-    else
+    elseif mode == 2 then
         out = inlinedRemote(remote, {[1] = getRootSafe(true)})
+    elseif mode == 3 then
+        out = inlinedRemote(remote, {[1] = {[1] = getRootSafe(false).Position}})
+    else
+        out = inlinedRemote(remote, {[1] = {[1] = getRootSafe(true)}})
     end
-    if out ~= nil then
-        print("Output: " .. out)
-    end
+    if out ~= nil then print("Output: " .. out) end
 end
