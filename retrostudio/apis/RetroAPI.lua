82264c1bf3326681ecf51cd63cde2f3c312e7a04
Anonymous <>
2025-01-16 15:42:41 +0900

    retroapi

---
+++ retrostudio/apis/RetroAPI.lua
@@ -0,0 +1,33 @@
+local retro = {}
+retro.__index = retro
+
+local RS:ReplicatedStorage = game:GetService("ReplicatedStorage")
+local retroRemote = RS:FindFirstChild("RemoteEvents")
+
+function retro.Teleport(id:number)
+    id = id or 5846386835
+    RS:FindFirstChild("TeleportEvent"):FireServer(unpack({[1] = id}))
+end
+function retro.Join(id:string, gametype:string, isStudio:boolean)
+    id = id or "0"
+    gametype = gametype or "solo"
+    isStudio = isStudio or false
+
+    if retroRemote then
+        retroRemote:FindFirstChild("Request" .. (isStudio and "Player" or "Studio") .. "Teleport"):FireServer(unpack({
+            [1] = id,
+            [2] = retroRemote
+        }))
+    end
+end
+
+function retro.ChangeBody(bodyType:string, color:Color3)
+    if retroRemote then
+        retroRemote:WaitForChild("ChangeAvatarBodyPartColor"):FireServer(unpack({
+            [1] = bodyType,
+            [2] = color
+        }))
+    end
+end
+
+return retro
