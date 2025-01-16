f2041ec06e8de6a043530468a9c312d4f577a7cf
Anonymous <>
2025-01-16 18:14:03 +0900

    lololol

--- retrostudio/GiveEveryBadge.lua
+++ retrostudio/GiveEveryBadge.lua
@@ -7,7 +7,7 @@ local playerName:string = PS.LocalPlayer.Name
 for _ = 0, length do
     LP.Badge:FireServer(unpack({
         [1] = playerName,
-        [2] = math.random(111111111, 999999999),
+        [2] = math.random(111111111, 999999999) .. "_" .. math.random(0, 99),
         [3] = playerName,
         [4] = "",
         [5] = ""
