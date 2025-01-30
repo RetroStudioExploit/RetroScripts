77a0dcee9e05cb312e8940eb81848581194d6c77
Anonymous <>
2025-01-31 04:20:32 +0900

    kek

--- misc/activeguis.lua
+++ misc/activeguis.lua
@@ -1,6 +1,13 @@
-for _, v in game:GetDescendants() do
+local guiName = "WorkspaceParts"
+local forceEnable = true
+
+local LP = game:GetService("Players").LocalPlayer
+local AdminGUI:ScreenGui = LP.PlayerGui:FindFirstChild(guiName)
+
+for _, v in AdminGUI:GetDescendants() do
     if v:IsA("TextButton") or v:IsA("ImageButton") then
         v.Active = true
         v.Visible = true
     end
 end
+if forceEnable then AdminGUI.Enabled = true end
