52169d20bc44acbf8dee213159f6ce8ca86209f1
Anonymous <>
2025-02-02 15:11:35 +0900

    cheating fnf

---
+++ retrostudio/SquidGame/GlassMaker.lua
@@ -0,0 +1,19 @@
+
+local glasses:table = {}
+for _, v in workspace:GetChildren() do
+    if v.Name == "GlassPannel" then
+        table.insert(glasses, v)
+    end
+end
+
+for _, v in pairs(glasses) do
+    if v.Safe.Value then
+        local sb = Instance.new("SelectionBox", v)
+        sb.Adornee = v
+        sb.Color3 = Color3.fromRGB(0, 255, 0)
+    else
+        local sb = Instance.new("SelectionBox", v)
+        sb.Adornee = v
+        sb.Color3 = Color3.fromRGB(255, 0, 0)
+    end
+end
