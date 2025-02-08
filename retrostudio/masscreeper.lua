1bd4bc3d5430b02fcf29a4c00d79204635a49fa6
Anonymous <>
2025-02-09 06:34:00 +0900

    whoops

--- retrostudio/masscreeper.lua
+++ retrostudio/masscreeper.lua
@@ -1,5 +1,3 @@
-local Events = game:GetService("Lighting"):WaitForChild("Events")
-
 function getInstance(name:string, instanceName:string)
     for _, even in game:GetDescendants() do
         if even.Name == name and even:IsA(instanceName) then
@@ -12,11 +10,5 @@ end
 local EE = getInstance("ExplosionEvent", "RemoteEvent")
 for _, player in game:GetService("Players"):GetPlayers() do
     local work_plr = workspace:FindFirstChild(player.Name)
-    if work_plr ~= nil then
-        EE:FireServer(unpack({
-            [1] = {
-                [1] = work_plr
-            }
-        }))
-    end
+    if work_plr ~= nil then EE:FireServer(unpack({[1] = {[1] = work_plr}})) end
 end
