da53423f9bbc52d0bb0917c9b7e12499f2859f7b
Anonymous <>
2025-02-11 10:20:53 +0900

    lol fuck you pineapple

--- retrostudio/Skyblock/SkyblockApi.lua
+++ retrostudio/Skyblock/SkyblockApi.lua
@@ -1,9 +1,33 @@
 local skyblock = {}
 skyblock.__index = skyblock
 
-function skyblock.Build(pos:Vector3, from, to)
-    local remote:RemoteEvent = workspace.BlockUpdate
-    remote:FireServer(unpack({[1] = {[1] = pos, [2] = from, [3] = to, [4] = false}}))
+function getBlocks(storageType:string):table
+    local tools = {}
+    local storage = (storageType == "lighting" and game.Lighting or workspace)
+    for _, v in storage:GetDescendants() do
+        if v.Name == "Place" and v:IsA("RemoteEvent") then
+            if v.Parent:IsA("Tool") then
+                table.insert(tools, v)
+            end
+        end
+    end
+    return tools
+end
+
+function skyblock.Build(pos:Vector3)
+    for _, tools in pairs(getBlocks("workspace")) do
+        tools:FireServer(unpack({
+            [1] = {
+                [1] = pos,
+                [2] = true
+            }
+        }))
+    end
+end
+function skyblock.BuildOld(pos:Vector3, from, to)
+    warn("Use Build instead.")
+    -- local remote:RemoteEvent = workspace.BlockUpdate
+    -- remote:FireServer(unpack({[1] = {[1] = pos, [2] = from, [3] = to, [4] = false}}))
 end
 
 function skyblock.BreakBlock(k:Part, DestroyPercent:number, mode:number, destroyed:boolean)
