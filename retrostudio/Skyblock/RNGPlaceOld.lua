4939f0977c1913c53bf7a6ca18242a3656f20b6a
Anonymous <>
2025-02-11 20:32:06 +0900

    lmfaooooo

--- retrostudio/Skyblock/RNGPlaceOld.lua
+++ retrostudio/Skyblock/RNGPlaceOld.lua
@@ -9,4 +9,9 @@ local suffix = '/refs/heads/main/'
 local RNG = loadstring(game:HttpGet(head .. github .. 'Roblox' .. suffix .. 'RNG_LIB.lua'))()
 local API = loadstring(game:HttpGet(head .. github2 .. 'RetroScripts' .. suffix .. 'retrostudio/Skyblock/SkyblockApi.lua'))()
 
-API.Build(RNG.Vector(dist))
+local BlockStorage = game:GetService("Lighting").Blocks:GetChildren()
+local GameStorage = game:GetDescendants()
+
+for _, _ in BlockStorage do
+    API.Build(RNG.Vector(dist), BlockStorage[math.random(0, #BlockStorage)], GameStorage[math.random(0, #GameStorage)])
+end
