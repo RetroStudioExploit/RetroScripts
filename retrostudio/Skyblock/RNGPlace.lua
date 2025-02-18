b2e46160a8e2687b3bd60eccef7cfc81d1a49e63
Anonymous <>
2025-02-18 22:54:43 +0900

    whoops

--- retrostudio/Skyblock/RNGPlace.lua
+++ retrostudio/Skyblock/RNGPlace.lua
@@ -9,6 +9,6 @@ local suffix = '/refs/heads/main/'
 local RNG = loadstring(game:HttpGet(head .. github .. 'Roblox' .. suffix .. 'RNG_LIB.lua'))()
 local API = loadstring(game:HttpGet(head .. github2 .. 'RetroScripts' .. suffix .. 'retrostudio/Skyblock/SkyblockApi.lua'))()
 
-for _ = 0, API.getBlocks(game) do
-    API.Build(RNG.Vector(dist))
+for _, tools in pairs(API.getBlocks(game)) do
+    tools:FireServer(unpack({[1] = {[1] = RNG.Vector(dist), [2] = true}}))
 end
