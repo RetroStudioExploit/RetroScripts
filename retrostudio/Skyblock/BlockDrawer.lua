d75b32f0793e62f334159d88d05d42cbb486e677
Anonymous <>
2025-03-02 10:59:58 +0900

    alright

--- retrostudio/Skyblock/BlockDrawer.lua
+++ retrostudio/Skyblock/BlockDrawer.lua
@@ -1,6 +1,7 @@
 local word = "nazi"
 local dist = 2
 local offset = Vector3.new(0, 0, 0)
+local modes = 1
 
 local head = 'https://raw.githubusercontent.com/'
 local github = 'RetroStudioExploit/'
@@ -10,20 +11,28 @@ local API = loadstring(game:HttpGet(head .. github .. 'RetroScripts' .. suffix .
 local ARTS = loadstring(game:HttpGet(head .. github .. 'RetroScripts' .. suffix .. 'Globals/CframeArts.lua'))()
 
 local PS = game:GetService("Players")
-local BlockStorage = game:GetService("Lighting").Blocks:GetChildren()
-
-local choosenBlock = BlockStorage[math.random(0, #BlockStorage)]
-
 function getRoot(char):Part
 	return char:FindFirstChild('HumanoidRootPart') or char:FindFirstChild('Torso') or char:FindFirstChild('UpperTorso')
 end
 
-for _, player in PS:GetPlayers() do
-	for _, v in pairs(ARTS.slurs[word]) do
-		local chr = getRoot(player.Character)
-		if chr == nil then continue end
+function buildBlock(tools:RemoteEvent, pos)
+	if modes == 1 then
+        tools:FireServer(unpack({[1] = {[1] = pos, [2] = true}}))
+    elseif modes == 2 then
+        tools:FireServer(unpack({[1] = {[1] = pos}}))
+    else
+        tools:FireServer(unpack({[1] = {[1] = CFrame.new(pos, Vector3.zero)}}))
+    end
+end
 
-		local vec:Vector3 = Vector3.new((v.X * dist) + chr.Position.X, (v.Y * dist) + chr.Position.Y, chr.Position.Z) + offset
-		API.Build(vec)
+for _, player in PS:GetPlayers() do
+	for _, tools in pairs(API.getBlocks(game)) do
+		for _, v in pairs(ARTS.slurs[word]) do
+			local chr = getRoot(player.Character)
+			if chr == nil then continue end
+
+			local vec:Vector3 = Vector3.new((v.X * dist) + chr.Position.X, (v.Y * dist) + chr.Position.Y, chr.Position.Z) + offset
+			buildBlock(tools, vec)
+		end
 	end
 end
--- retrostudio/Skyblock/RNGPlace.lua
+++ retrostudio/Skyblock/RNGPlace.lua
@@ -1,5 +1,5 @@
 local dist = 1000
-local cframemode = false
+local modes = 1
 
 local head = 'https://raw.githubusercontent.com/'
 local github = 'system32unknown/'
@@ -11,8 +11,10 @@ local RNG = loadstring(game:HttpGet(head .. github .. 'Roblox' .. suffix .. 'RNG
 local API = loadstring(game:HttpGet(head .. github2 .. 'RetroScripts' .. suffix .. 'retrostudio/Skyblock/SkyblockApi.lua'))()
 
 for _, tools in pairs(API.getBlocks(game)) do
-    if not cframemode then
+    if modes == 1 then
         tools:FireServer(unpack({[1] = {[1] = RNG.Vector(dist), [2] = true}}))
+    elseif modes == 2 then
+        tools:FireServer(unpack({[1] = {[1] = RNG.Vector(dist)}}))
     else
         tools:FireServer(unpack({[1] = {[1] = RNG.CFrame(dist, 360)}}))
     end
